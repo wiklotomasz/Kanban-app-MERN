d42846a12e76aa4fdf9e434c1b416ad63745f37c
Beata Karolczak <karolczak.beata@gmail.com>
2018-05-07 14:59:51 +0200

    Add reducers

--- client/modules/Lane/LaneReducer.js
+++ client/modules/Lane/LaneReducer.js
@@ -1,14 +1,43 @@
 // Import Actions
-import {  } from './LaneActions';
+import { CREATE_LANE, UPDATE_LANE, DELETE_LANE } from './LaneActions';
+import { CREATE_NOTE, DELETE_NOTE } from '../Note/NoteActions';
 
 // Initial State
-const initialState = {};
+const initialState = [];
 
-const LaneReducer = (state = initialState, action) => {
+export default function lanes(state = initialState, action) {
   switch (action.type) {
+  	case CREATE_LANE:
+  		return [...state, action.lane];
+  	case UPDATE_LANE:
+  		return state.map(lane => {
+  			if (lane.id === action.id) {
+  				return Object.assign({}, lane, action.lane);
+  			}
+  			return lane;
+  		});
+  	case DELETE_LANE:
+  		return state.filter(lane => lane.id !== action.laneId);
+
+  	case CREATE_NOTE:
+		return state.map(lane => {
+		    if (lane.id === action.laneId) {
+		      const notes = [...lane.notes, action.note.id];
+		      return { ...lane, notes };
+		    }
+		    return lane;
+		 });
+	case DELETE_NOTE:
+		return state.map(lane => {
+			if(lane.id === action.laneId) {
+				const notes = [...lane.notes];
+				return notes.filter((note) => note.id !== action.noteId);
+			}
+			return lane;
+		});
     default:
       return state;
-  }
-};
+    }
+}
 
 export default LaneReducer;
--- client/modules/Note/NoteReducer.js
+++ client/modules/Note/NoteReducer.js
@@ -1,11 +1,19 @@
 // Import Actions
-import {  } from './NoteActions';
+import { CREATE_NOTE, UPDATE_NOTE, DELETE_NOTE } from './NoteActions';
 
 // Initial State
-const initialState = {};
+const initialState = [];
 
-const NoteReducer = (state = initialState, action) => {
+export default function notes = (state = initialState, action) => {
   switch (action.type) {
+  	case CREATE_NOTE:
+  		return [...state, action.note];
+  	case UPDATE_LANE:
+  		return state.map(note => {
+  			return note.id === action.id ? { ...note, ...action.note } : note;			
+  		});
+  	case DELETE_NOTE:
+  		return state.filter(note => note.id !== action.noteId);
     default:
       return state;
   }
